802d9a0da4063f544db026616377090e878486a8
Takadinha <61235565+Takadinha@users.noreply.github.com>
2021-05-23 22:01:55 -0300

    Delete

--- Codigos/Q0631-Sketch-Master/config.h
+++
@@ -1,6 +0,0 @@
-#define IO_USERNAME   "Haha9918273645"
-#define IO_KEY        "aio_ZQNM7423O41VNATUtu4HI7yaneEl"
-#define WIFI_SSID   "Quebrado"
-#define WIFI_PASS   "Adivinha"
-#include "AdafruitIO_WiFi.h"
-AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
